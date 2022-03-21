e891da10f4fba013a163b3c21460c9a05f69abf1
unknown <datdonguyen99@gmail.com>
2022-03-21 12:27:16 +0700

    err-price

--- ecommerce/pages/api/admin/products/index.js
+++ ecommerce/pages/api/admin/products/index.js
@@ -18,14 +18,35 @@ handler.post(async (req, res) => {
   const newProduct = new Product({
     name: "sample name",
     slug: "sample-slug-" + Math.random(),
-    image: "/images/shirt1.jpg",
-    price: 0,
+    price: "1940000",
     category: "sample category",
     brand: "sample brand",
     countInStock: 0,
-    description: "sample description",
     rating: 0,
     numReviews: 0,
+    processorAndMemoryFeatures: {
+      cache: "8MB",
+      clockSpeed: "2.4 GHz with Turbo Boost Upto 4.2 GHz",
+      graphicProcessor: "Intel Integrated Iris Xe",
+      processorBrand: "Intel",
+      processorGeneration: "11th Gen",
+      processorName: "Core i5",
+      processorVariant: "1135G7",
+      ram: "16 GB",
+      ramType: "DDR4",
+      ssd: "Yes",
+      ssdCapacity: "512 GB",
+    },
+    dimensions: {
+      dimensions: "299 x 210 x 14.9 mm",
+      weight: 1.26,
+    },
+    displayAndAudioFeatures: {
+      screenResolution: "2560 x 1600 Pixel",
+      screenSize: "33.02 cm (13 inch)",
+      screenType: "WQXGA IPS Anti-glare Display",
+      touchscreen: "Not available",
+    },
   });
 
   const product = await newProduct.save();
